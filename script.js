d41a431ecb19a830925b89a279189c3b760392b3
matheus1714 <matheussilvamartins1714@gmail.com>
2023-12-19 09:05:41 -0300

    create timer logic

---
+++ script.js
@@ -0,0 +1,14 @@
+const targetDate = new Date('2024-01-15')
+
+const updateTimer = () => {
+    const today = new Date()
+    const milisecondsToDays = 1000 * 60 * 60 * 24
+    const difference  = Math.floor((targetDate - today) / milisecondsToDays)
+
+    const timerElement = document.querySelector('#timer')
+    timerElement.textContent = difference.toString().padStart(2, '0')
+}
+
+updateTimer()
+
+setInterval(updateTimer, 1000)
